baf333058cfdcf8328b80f926b3d0c19bb835f20
marinakip <marinakipourou@hotmail.com>
2023-01-12 13:35:08 +0200

    Initial commit of Session-04 and Exercise1

---
+++ Session-04/Session-04/HelloName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Session_04
+{
+    public class HelloName
+    {
+        // A method that prints Hello and name in the same line.       
+        public void PrintHelloName()
+        {
+            string greeting = "Hello";
+            string name = "Marina";
+
+            Console.WriteLine(greeting + " " + name);
+        }
+
+    }
+}
---
+++ Session-04/Session-04/Program.cs
@@ -0,0 +1,12 @@
+﻿using Session_04;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Exercise 1: Write a C# program to print Hello and your name in the same line.
+        HelloName greeting = new HelloName();
+        greeting.PrintHelloName();
+
+    }
+}
