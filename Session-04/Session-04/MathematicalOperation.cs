bfb20a8e38e7400d3c60ab0f1972e25862b16ba0
marinakip <marinakipourou@hotmail.com>
2023-01-12 15:51:06 +0200

    Add MathimaticalOperation.cs that contains the first and the second operations for Exercise2

---
+++ Session-04/Session-04/MathematicalOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Session_04
+{
+    public class MathematicalOperation
+    {
+        // It calculates the operation −1 + (5 × 6)
+        public int calculateFirstOperation()
+        { 
+            int firstNumber = -1;
+            int secondNumber = 5;
+            int thirdNumber = 6;       
+            int result = firstNumber + (secondNumber * thirdNumber);
+            return result;
+        }
+
+        // It calculates the operation 38 + 5 𝑚𝑜𝑑 7
+        public int calculateSecondOperation()
+        {
+            int firstNumber = 38;
+            int secondNumber = 5;
+            int thirdNumber = 7;
+            int result = firstNumber + (secondNumber % thirdNumber);
+            return result;
+        }
+
+    }
+}
