51b62898aed7d63d0237928f931673ec6f2c0cb5
marinakip <marinakipourou@hotmail.com>
2023-01-13 15:30:42 +0200

    Initial commit for Session-05 Add StringReverse for Exercise 1

---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,20 @@
+﻿using Session_05;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        int[] exercises = { 1, 2, 3, 4, 5 };
+        int sessionNumber = 5;
+        Console.WriteLine($"SESSION-{sessionNumber:00} EXERCISES STARTING.... {Environment.NewLine}");
+
+        Console.WriteLine($"EXERCISE {exercises[0]} {Environment.NewLine}");
+        Console.Write($"Please give me your name to show it reversed: ");
+        string name = Console.ReadLine();
+        StringReverse reversedName = new StringReverse(name);
+        reversedName.PrintReverseString();
+
+        Console.ReadLine();
+    }
+}
---
+++ Session-05/Session-05/StringReverse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Session_05
+{
+    public class StringReverse
+    {
+        private string sequenceOfCharacters;
+
+        public StringReverse(string sequenceOfCharacters){
+            this.sequenceOfCharacters = sequenceOfCharacters;
+        }
+
+        private char[] GetStringArray(){
+            char[] ArrayofAString = new char[sequenceOfCharacters.Length];
+            for (int i = 0;  i < sequenceOfCharacters.Length; i++){
+                ArrayofAString[i] = sequenceOfCharacters[i];
+            }
+            return ArrayofAString;
+        }
+
+        private char[] GetReversedArray(){
+            char[] forwardCharacterArray = GetStringArray();
+            char[] backwardCharacterArray = new char [forwardCharacterArray.Length];
+            int j = 0;
+            for (int i = forwardCharacterArray.Length - 1; i >= 0; i--){
+                backwardCharacterArray[j] = forwardCharacterArray[i];
+                j++;                
+            }
+            return backwardCharacterArray;
+        }
+        public void PrintReverseString(){
+            char[] reversedArray = GetReversedArray();    
+            foreach (char character in reversedArray){
+                Console.Write(character);
+            }
+            Console.WriteLine($"{Environment.NewLine}");
+        }
+    }
+}
