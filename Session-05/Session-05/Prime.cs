1c796a18dc1b558e325b61ab9c28594c614ab495
marinakip <marinakipourou@hotmail.com>
2023-01-15 04:58:48 +0200

    Add prime numbers Exercise 3 -Session-05

---
+++ Session-05/Session-05/Prime.cs
@@ -0,0 +1,31 @@
+﻿internal class Prime
+{
+    public Prime()
+    {
+    }
+
+    public void CalculateAllPrimes(int primesUpperBound){
+        
+        for (int i = 1; i <= primesUpperBound; i++){
+            if (IsPrime(i)){
+                Console.Write($"{i} ");
+            }
+        }
+          
+    }
+
+    public bool IsPrime(int number)
+    {
+        if (number == 0 || number == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Session_05;
+using System.ComponentModel;
 using System.Net.Http.Headers;
 
 internal class Program
@@ -11,14 +12,14 @@ internal class Program
         int sessionNumber = 5;
         Console.WriteLine($"SESSION-{sessionNumber:00} EXERCISES STARTING.... {Environment.NewLine}");
 
+        
+        // Exercise 1: Write a C# program that reverses a given string (your name).
 
-        //// Exercise 1: Write a C# program that reverses a given string (your name).
-
-        //Console.WriteLine($"EXERCISE {exercises[0]} {Environment.NewLine}");
-        //Console.Write($"Please insert your name to show it reversed: ");
-        //string name = Console.ReadLine();               //TODO: Check if it is a string, should not take numbers
-        //StringReverse reversedName = new StringReverse(name);
-        //reversedName.PrintReverseString();
+        Console.WriteLine($"EXERCISE {exercises[0]} {Environment.NewLine}");
+        Console.Write($"Please insert your name to show it reversed: ");
+        string name = Console.ReadLine();               //TODO: Check if it is a string, should not take numbers
+        StringReverse reversedName = new StringReverse(name);
+        reversedName.PrintReverseString();
 
         // Exercise 2: Write a C# program that asks the user for an integer (n) and gives them
         // the possibility to choose between computing the sum and computing the product of 1,…,n.
@@ -30,12 +31,10 @@ internal class Program
         Console.Write($"2. Product {Environment.NewLine}");
         string selection = Console.ReadLine();    //TODO: Check if it is an integer
         int selectionNumber = Convert.ToInt32(selection);
+
         Console.WriteLine("Please insert n value to calculate the Sum of 1,...,n :");
         string number = Console.ReadLine();    //TODO: Check if it is an integer and not negative
         int n = Convert.ToInt32(number);
-
-        
-
         if (selectionNumber == 1){
             Sum sum = new Sum();
             int resultSum = sum.CalculateSum(n);
@@ -45,11 +44,20 @@ internal class Program
             Product product = new Product();
             int resultProduct = product.CalculateProduct(n);
             Console.WriteLine($"The product of {n} numbers is: {resultProduct}");
-        }             
+        }
 
-
-
-        //Console.WriteLine();
+        
+        // Exercise 3: Write a C# program that asks the user for an integer (n) and finds all the prime numbers from 1 to n.
+        
+        Console.Write("Please insert n value to find all the prime numbers from 1 to n:");
+        string input = Console.ReadLine();    //TODO: Check if it is an integer and not negative
+        int primesUpperBound = Convert.ToInt32(input);       //TODO: REFACTOR EXTRACT METHOD FOR INPUT and if string is number and not negative
+        Console.WriteLine($"The primes from 1 to {primesUpperBound} are:");
+       
+        Prime primeNumber = new Prime();
+        primeNumber.CalculateAllPrimes(primesUpperBound);
+      
+      
 
         Console.ReadLine();
        
