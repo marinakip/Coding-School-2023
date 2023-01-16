b795e1816979c94f73d05432a2cee4c3b868461f
marinakip <marinakipourou@hotmail.com>
2023-01-16 15:04:41 +0200

    Add Institute class

--- Session-06/Session-06/Institute.cs
+++ Session-06/Session-06/Institute.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Session_06
 {
-    public class Institute
-    {
+    public class Institute {
+        // Properties
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public int YearsInService { get; set; }
+
+        // Constructors
+        public Institute() { }
+
+        public Institute(Guid id) {
+            ID = id;
+        }
+        public Institute(Guid id, string name) {
+            ID = id;
+            Name = name;
+        }
+        public Institute(Guid id, string name, int yearsInService) {
+            ID = id;
+            Name = name;
+            YearsInService = yearsInService;
+        }
+
+        // Methods
+        public void GetName() { }
+
+        public void SetName(string name) { }
     }
+}
 }
