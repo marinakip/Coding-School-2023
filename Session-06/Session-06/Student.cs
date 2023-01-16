d0ea68bdd0da87e6adeb617b49988907d707f072
marinakip <marinakipourou@hotmail.com>
2023-01-16 14:47:45 +0200

    Add Student class

--- Session-06/Session-06/Student.cs
+++ Session-06/Session-06/Student.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Session_06
 {
-    public class Student
-    {
+    public class Student : Person {
+
+        // Properties
+        public int RegistrationNumber { get; set; }
+        public Course[]? Courses { get; set; }    
+
+        // Constructors
+        public Student() { }
+
+        public Student(int registrationNumber) {
+            RegistrationNumber = registrationNumber;
+        }
+        public Student(int registrationNumber, Course[]? courses) {
+            RegistrationNumber = registrationNumber;
+            Courses = courses;
+        }
+
+
+        // Methods
+        public void Attend(Course course, DateTime date) { }    
+        public void WriteExam(Course course, DateTime date) { }    
+
+
     }
 }
