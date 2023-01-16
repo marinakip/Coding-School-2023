9f5de009d476acd3f4369e18a463e9475844e29d
marinakip <marinakipourou@hotmail.com>
2023-01-16 15:56:22 +0200

    Add Schedule class

--- Session-06/Session-06/Schedule.cs
+++ Session-06/Session-06/Schedule.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Session_06
 {
-    public class Schedule
-    {
+    public class Schedule {
+
+        // Properties
+        public Guid ID { get; set; }
+        public Guid CourseID { get; set; }
+        public Guid Professor { get; set; } 
+        public DateTime Calendar { get; set; }
+
+        // Constructors
+        public Schedule() {
+
+        }
+        public Schedule(Guid iD) {
+            ID = iD;
+        }
+        public Schedule(Guid iD, Guid courseID) {
+            ID = iD;
+            CourseID = courseID;
+        }
+        public Schedule(Guid iD, Guid courseID, Guid professor) {
+            ID = iD;
+            CourseID = courseID;
+            Professor = professor;
+        }
+        public Schedule(Guid iD, Guid courseID, Guid professor, DateTime calendar) {
+            ID = iD;
+            CourseID = courseID;
+            Professor = professor;
+            Calendar = calendar;
+        }
     }
 }
