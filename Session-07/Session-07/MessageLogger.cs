1efef434b252891c84295d4a99cdfad5a4c71be5
marinakip <marinakipourou@hotmail.com>
2023-01-17 15:33:39 +0200

    Add MessageLogger
    Creates MessageLogger properties, constructors and methods

--- Session-07/Session-07/MessageLogger.cs
+++ Session-07/Session-07/MessageLogger.cs
@@ -1,4 +1,22 @@
 ﻿namespace Session_07 {
     public class MessageLogger {
+
+        // Properties
+        public Message[] Messages { get; set; } 
+
+        // Constructors
+        //public MessageLogger() { } // Unessesary 
+        public MessageLogger(Message[] messages) {
+            Messages = messages;    
+        }
+
+        // Methods
+        public void ReadAll() { //should return Message[]
+
+        }
+        public void Clear() {
+
+        }
+        public void Write(Message message) { }
     }
 }
