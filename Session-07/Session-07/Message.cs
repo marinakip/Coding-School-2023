9e04893bd0fcc382cb8303a0a16debc90be69310
marinakip <marinakipourou@hotmail.com>
2023-01-17 15:45:44 +0200

    Add Message class
    Created Message class with properties and constructors

---
+++ Session-07/Session-07/Message.cs
@@ -0,0 +1,22 @@
+﻿namespace Session_07 {
+    public class Message {
+
+        // Properties
+        public Guid ID { get; set; }
+        public DateTime TimeStamp { get; set; } 
+        public string? MessageText { get; set; }   //FIX: Renamed because of error CS0542: member names 
+                                                   //     cannot be the same as their enclosing type
+        // Constructors
+        // public Message() {} // Unnessesary
+        public Message(Guid id, DateTime date, string message) {
+            ID = id;    
+            TimeStamp = date;
+            MessageText = message;
+        }
+
+    }
+
+
+
+
+}
