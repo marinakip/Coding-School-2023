b1d137d0d8decb568c656963555ca82ad2097a42
marinakip <marinakipourou@hotmail.com>
2023-01-17 14:40:48 +0200

    Add ActionResponse class
    Created ActionResponse class with properties and constructors

---
+++ Session-07/Session-07/ActionResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    public class ActionResponse {
+
+        // Properties
+        public Guid RequestID { get; set; }
+        public Guid ResponseID { get; set; }
+        public string? Output { get; set; }
+
+        // Constructors
+        //public ActionResponse() { }  // Unnessesary
+        public ActionResponse(Guid requestID, Guid responseID) {
+            RequestID = requestID;
+            ResponseID = responseID; 
+        }
+        public ActionResponse(Guid requestID, Guid responseID, string? output) {
+            RequestID = requestID;
+            ResponseID = responseID;
+            Output = output;
+        }   
+    }
+}
