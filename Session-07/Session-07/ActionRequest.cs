6112b91d8a8a592abf9f5872c452ac0e1f328256
marinakip <marinakipourou@hotmail.com>
2023-01-17 14:30:04 +0200

    Initial Commit - Add ActionRequest
    Add ActionRequest class with properties and constructors

---
+++ Session-07/Session-07/ActionRequest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    public enum ActionEnum {
+        Convert,
+        Uppercase,
+        Reverse
+    }
+    public class ActionRequest {
+
+        // Properties
+        public Guid RequestID { get; set; }
+        public string? Input { get; set; }
+        public ActionEnum Action { get; set; }
+
+        // Constructors
+
+        // Unnessary constructors
+        //public ActionRequest() { }
+        //
+        //public ActionRequest(Guid requestID) { 
+        //    RequestID = requestID;
+        //}
+
+        //public ActionRequest(Guid requestID, string input) {
+        //    RequestID = requestID;
+        //    Input = input;
+        //}
+
+        // Only this construtor is actually needed, the others don't make sense
+        public ActionRequest(Guid requestID, string input, ActionEnum enumerator) {
+            RequestID = requestID;
+            Input = input;
+            Action = enumerator;    
+        }
+    }
+}
