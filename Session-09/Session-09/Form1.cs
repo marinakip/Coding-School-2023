6406fb595ec8ad63749147c223ed171365730ae8
marinakip <marinakipourou@hotmail.com>
2023-01-19 14:23:05 +0200

    Initial commit UI created for Session-08
    Created UI for calculator, small changes were made to the default UI for for aesthetic reasons. Background color, fonts, font color and size, button colors and text box color and font were changed.

---
+++ Session-09/Session-09/Form1.cs
@@ -0,0 +1,27 @@
+namespace Session_09 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
