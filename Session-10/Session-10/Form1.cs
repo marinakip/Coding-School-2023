8e1fd9c3c79c00c5ad4c2932401737e63ff40ed6
marinakip <marinakipourou@hotmail.com>
2023-01-21 14:14:33 +0200

    Add SetControlProperties and refactored it
    Control properties is used at form load to clear grid from previous generation. It was refactored using Generics. Also for student's registration number was initialized using random.

--- Session-10/Session-10/Form1.cs
+++ Session-10/Session-10/Form1.cs
@@ -1,10 +1,17 @@
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UniversityEntitiesLibrary;
 
 namespace Session_10 {
     public partial class Form1 : Form {
+
+        #region Properties
         private University _university;
         private List<Student> _students;
+        private List<Grade> _grades;
+        private List<Course> _courses;
+        private List<Schedule> _scheduledCourses;
+        #endregion Properties
         public Form1() {
             InitializeComponent();
         }
@@ -40,28 +47,50 @@ namespace Session_10 {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            //SetControlProperties();            
             PopulateStudents();
+            PopulateGrades();
+            PopulateCourses();
+            PopulateScheduledCourses();
+
+        }
+
+        private void SetControlProperties() {
+            ClearGridview(grvStudents, _students);
+            ClearGridview(grvGrades, _grades);
+            ClearGridview(grvCourses, _courses);
+            ClearGridview(grvScheduledCourses, _scheduledCourses);
+          
+        }
+
+        private void ClearGridview<T>(DataGridView gridView, List<T> list) {
+            gridView.AutoGenerateColumns = false;
+            gridView.DataSource = list;
         }
 
         private void PopulateStudents() {
             _students = new List<Student>();
             Random random = new Random();
-            
 
-            //It has id, name age registrationNumber
+            // Gender, Undergraduate and UniversityID don't exist in the original class diagram
+            // For this reason, they are not initialized here
+
+            //Student has only id, name, age, registrationNumber
+
             Student student1 = new Student() {
                 ID = Guid.NewGuid(),    
-                Name = "Jane Doe", //TODO: Maybe Split it later
+                Name = "Jane Doe", //TODO: Maybe needs split later
                 Age = 19,
-                RegistrationNumber= random.Next()
-                //UniversityID = unis[0].ID,
+                RegistrationNumber= random.Next(10000000, 99999999)   // needs an integer so random.Next() is used
+                //UniversityID = unis[0].ID,                          // min and max value is used as a length check,
+                                                                      // so the random registrationNumber has the same length for all students
             };
 
             _students.Add(student1);
 
             Student student2 = new Student() {
                 ID = Guid.NewGuid(),
-                Name = "John Dewey", //TODO: Maybe Split it later
+                Name = "John Dewey", 
                 Age = 21,
                 RegistrationNumber = random.Next()
             };
@@ -69,14 +98,28 @@ namespace Session_10 {
 
             Student student3 = new Student() {
                 ID = Guid.NewGuid(),
-                Name = "Paulo Pedersen", //TODO: Maybe Split it later
+                Name = "Paulo Pedersen", 
                 Age = 20,
-                RegistrationNumber = random.Next()
-                //UniversityID = unis[0].ID,                
+                RegistrationNumber = random.Next()        
             };
             _students.Add(student3);
             //_students.ToArray();
-            grvStudents.DataSource= _students;  
+
+            grvStudents.DataSource = _students;  
+
+          
+        }
+
+
+        private void PopulateGrades() {
+
+        }
+
+        private void PopulateCourses() {
+
+        }
+
+        private void PopulateScheduledCourses() {
 
         }
 
