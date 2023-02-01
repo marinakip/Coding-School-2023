d196888893927180452f6ada92169be3976fa216
marinakip <marinakipourou@hotmail.com>
2023-02-01 03:35:40 +0200

    Add IEntityRepository

---
+++ Session-16/Session-16.Orm/Repositories/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Session_16.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_16.Orm.Repositories {
+    public interface IEntityRepository<TEntity>
+        where TEntity : IEntityBase {
+        IList<TEntity> GetAll();
+        TEntity? GetById(int id);
+        void Add(TEntity entity);
+        void Update(int id, TEntity entity);
+        void Delete(int id);
+    }
+}
