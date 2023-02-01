31cf247c2a093432d364e3f79ad2d33de8ba1cbb
marinakip <marinakipourou@hotmail.com>
2023-02-01 03:37:19 +0200

    Add CustomerRepository - Not Implemented yet
    Add CustomerRepository - Not Implemented yet , add comment todos for future steps

---
+++ Session-16/Session-16.Orm/Repositories/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using Session_16.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_16.Orm.Repositories {
+
+    //TODO: Add inheritance from EntityBase in all model classes, remove IDs
+    //TODO: Add inheritance from IEntityRepository and create all repos
+    //TODO: Add functionality in Form1 with repos
+    public class CustomerRepository {  //: IEntityRepository<Customer> {
+        public void Add(Customer entity) {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id) {
+            throw new NotImplementedException();
+        }
+
+        public IList<Customer> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public Customer? GetById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Customer entity) {
+            throw new NotImplementedException();
+        }
+    }
+}
