92cb4ec834f19839a7576d81a6cdebef7d782737
marinakip <marinakipourou@hotmail.com>
2023-02-01 03:21:03 +0200

    Add interface IEntityBase and abstract class EntityBase

---
+++ Session-16/Session-16.Model/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_16.Model {
+    public abstract class EntityBase : IEntityBase {
+        public int Id { get; set; }
+    }
+}
