fc55e52b23d29050f814174a16701786880ca1bc
marinakip <marinakipourou@hotmail.com>
2023-02-02 16:25:12 +0200

    Removed some comments

--- Session-16/Session-16.Model/Transaction.cs
+++ Session-16/Session-16.Model/Transaction.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 
 namespace Session_16.Model {
 
-    /* In Each Transaction we have 1 Employee, 1 Customer that can buy 1 Pet but can buy many PetFoods, so we use List<PetFood> PetFoods */
     public class Transaction {
 
         #region Properties
