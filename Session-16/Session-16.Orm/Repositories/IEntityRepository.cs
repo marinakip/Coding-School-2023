b5424901043d66e39ecb333cdb1623a4b6ecfab8
marinakip <marinakipourou@hotmail.com>
2023-02-03 18:19:17 +0200

    Add PetRepository

--- Session-16/Session-16.Orm/Repositories/IEntityRepository.cs
+++ Session-16/Session-16.Orm/Repositories/IEntityRepository.cs
@@ -6,11 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Session_16.Orm.Repositories {
-    public interface IEntityRepository<TEntity> { 
+    public interface IEntityRepository<TEntity> {
         //where TEntity : IEntityBase {
-        IList<TEntity> GetAll();
-        TEntity? GetById(Guid id);
         void Add(TEntity entity);
+        IList<TEntity> GetAll();
+        TEntity? GetById(Guid id);   
         void Update(Guid id, TEntity entity);
         void Delete(Guid id);
     }
---
+++ Session-16/Session-16.Orm/Repositories/PetRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Session_16.Model;
+using Session_16.Orm.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_16.Orm.Repositories {
+    public class PetRepository : IEntityRepository<Pet> {
+        public void Add(Pet entity) {
+            using var context = new AppDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public IList<Pet> GetAll() {
+            using var context = new AppDbContext();
+            return (IList<Pet>)context.Pets;
+        }
+
+        public Pet? GetById(Guid id) {
+            using var context = new AppDbContext();
+            return context.Pets.Where(pet => pet.PetID == id)
+                .Include(pet => pet.TransactionLine).SingleOrDefault();
+        }
+
+        public void Update(Guid id, Pet entity) {
+            using var context = new AppDbContext();
+            var dbPet = context.Pets.Where(pet => pet.PetID == id).SingleOrDefault();
+            if (dbPet is null)
+                return;
+            dbPet.Breed = entity.Breed;
+            dbPet.Type = entity.Type;
+            dbPet.Status = entity.Status;
+            dbPet.Price= entity.Price;
+            dbPet.Cost = entity.Cost;           
+            context.SaveChanges();
+        }
+        public void Delete(Guid id) {
+            using var context = new AppDbContext();
+            var dbPet = context.Pets.Where(pet => pet.PetID == id).SingleOrDefault();
+            if (dbPet is null)
+                return;
+            context.Remove(dbPet);
+            context.SaveChanges();
+        }
+    }
+}
