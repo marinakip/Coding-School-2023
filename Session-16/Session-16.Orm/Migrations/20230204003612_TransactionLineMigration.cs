af9918b911cf6b66e7fab04034440bc3e0301e69
marinakip <marinakipourou@hotmail.com>
2023-02-04 02:45:59 +0200

    Add TransactionLineMigration and AppDbModelSnapshot

---
+++ Session-16/Session-16.Orm/Migrations/20230204003612_TransactionLineMigration.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Session16.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionLineMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PetPrice",
+                table: "TransactionLine",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PetFoodPrice",
+                table: "TransactionLine",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PetFoodCost",
+                table: "TransactionLine",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "LineTotal",
+                table: "TransactionLine",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PetPrice",
+                table: "TransactionLine",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PetFoodPrice",
+                table: "TransactionLine",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "PetFoodCost",
+                table: "TransactionLine",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "LineTotal",
+                table: "TransactionLine",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+        }
+    }
+}
