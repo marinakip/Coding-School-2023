c227906ddf4f3327bd1348249248debcb4528edc
marinakip <marinakipourou@hotmail.com>
2023-02-05 02:17:08 +0200

    Add Serializer

---
+++ Session-16/Session-16.Storage/Serializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Session_16.Storage {
+    public class Serializer {
+
+        public void SerializeToFile(object obj, string fileName) {
+
+            string jsonString = JsonSerializer.Serialize(obj);
+
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        public T DeserializeFromFile<T>(string fileName) {
+
+            string jsonString = File.ReadAllText(fileName);
+
+            T? obj = JsonSerializer.Deserialize<T>(jsonString);
+
+            return obj;
+
+        }
+    }
+}
