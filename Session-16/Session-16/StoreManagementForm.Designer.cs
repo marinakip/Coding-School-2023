cf824611541e7a2b62823e6f84a9a1281f8a66e2
marinakip <marinakipourou@hotmail.com>
2023-02-05 16:52:04 +0200

    Modified StoreMangementForm - Added grids
    Added grids for Inventory (CRUD), Employees (CRUD), MonthlyLedger and PetReport (READ ONLY) and DataGridViews for Monthly Ledger and PetReport Charts, so that store management has a quicker view of store's data.

--- Session-16/Session-16/StoreManagementForm.Designer.cs
+++ Session-16/Session-16/StoreManagementForm.Designer.cs
@@ -29,13 +29,32 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grvInventory = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lbOrders = new System.Windows.Forms.Label();
             this.logo = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.lbInventory = new System.Windows.Forms.Label();
+            this.lbMonthlyLedger = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbPetReport = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lbEmployees = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.lbMonthlyLedgerChart = new System.Windows.Forms.Label();
+            this.lbPetReportChart = new System.Windows.Forms.Label();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grvInventory)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
             this.SuspendLayout();
             // 
             // btnOk
@@ -58,7 +77,7 @@
             this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(83)))), ((int)(((byte)(174)))));
             this.btnUpdate.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnUpdate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnUpdate.Location = new System.Drawing.Point(19, 569);
+            this.btnUpdate.Location = new System.Drawing.Point(19, 270);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(80, 26);
             this.btnUpdate.TabIndex = 46;
@@ -71,7 +90,7 @@
             this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(70)))), ((int)(((byte)(84)))));
             this.btnDelete.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnDelete.Location = new System.Drawing.Point(123, 569);
+            this.btnDelete.Location = new System.Drawing.Point(124, 270);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(80, 26);
             this.btnDelete.TabIndex = 45;
@@ -84,7 +103,7 @@
             this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(113)))), ((int)(((byte)(177)))));
             this.btnAdd.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnAdd.Location = new System.Drawing.Point(815, 569);
+            this.btnAdd.Location = new System.Drawing.Point(327, 270);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(80, 26);
             this.btnAdd.TabIndex = 44;
@@ -105,14 +124,14 @@
             this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // dataGridView1
+            // grvInventory
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(19, 66);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(876, 497);
-            this.dataGridView1.TabIndex = 42;
+            this.grvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvInventory.Location = new System.Drawing.Point(19, 111);
+            this.grvInventory.Name = "grvInventory";
+            this.grvInventory.RowTemplate.Height = 25;
+            this.grvInventory.Size = new System.Drawing.Size(388, 153);
+            this.grvInventory.TabIndex = 42;
             // 
             // pictureBox1
             // 
@@ -140,12 +159,162 @@
             this.logo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
-            this.logo.Location = new System.Drawing.Point(919, 12);
+            this.logo.Location = new System.Drawing.Point(1044, 12);
             this.logo.Name = "logo";
-            this.logo.Size = new System.Drawing.Size(334, 124);
+            this.logo.Size = new System.Drawing.Size(209, 65);
             this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.logo.TabIndex = 39;
             this.logo.TabStop = false;
+            // 
+            // lbInventory
+            // 
+            this.lbInventory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbInventory.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbInventory.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbInventory.Location = new System.Drawing.Point(19, 85);
+            this.lbInventory.Name = "lbInventory";
+            this.lbInventory.Size = new System.Drawing.Size(175, 23);
+            this.lbInventory.TabIndex = 48;
+            this.lbInventory.Text = "Inventory";
+            // 
+            // lbMonthlyLedger
+            // 
+            this.lbMonthlyLedger.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbMonthlyLedger.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbMonthlyLedger.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbMonthlyLedger.Location = new System.Drawing.Point(443, 85);
+            this.lbMonthlyLedger.Name = "lbMonthlyLedger";
+            this.lbMonthlyLedger.Size = new System.Drawing.Size(175, 23);
+            this.lbMonthlyLedger.TabIndex = 50;
+            this.lbMonthlyLedger.Text = "Monthly Ledger";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(443, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(388, 153);
+            this.dataGridView1.TabIndex = 49;
+            // 
+            // lbPetReport
+            // 
+            this.lbPetReport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbPetReport.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbPetReport.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbPetReport.Location = new System.Drawing.Point(865, 85);
+            this.lbPetReport.Name = "lbPetReport";
+            this.lbPetReport.Size = new System.Drawing.Size(175, 23);
+            this.lbPetReport.TabIndex = 52;
+            this.lbPetReport.Text = "Pet Report";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(865, 111);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(388, 153);
+            this.dataGridView2.TabIndex = 51;
+            // 
+            // lbEmployees
+            // 
+            this.lbEmployees.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbEmployees.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbEmployees.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbEmployees.Location = new System.Drawing.Point(19, 323);
+            this.lbEmployees.Name = "lbEmployees";
+            this.lbEmployees.Size = new System.Drawing.Size(175, 23);
+            this.lbEmployees.TabIndex = 57;
+            this.lbEmployees.Text = "Employees";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(83)))), ((int)(((byte)(174)))));
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(19, 559);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 26);
+            this.button1.TabIndex = 56;
+            this.button1.Text = "UPDATE";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(70)))), ((int)(((byte)(84)))));
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(124, 559);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 26);
+            this.button2.TabIndex = 55;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(113)))), ((int)(((byte)(177)))));
+            this.button3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(327, 559);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 26);
+            this.button3.TabIndex = 54;
+            this.button3.Text = "ADD";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(19, 349);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 25;
+            this.dataGridView3.Size = new System.Drawing.Size(388, 203);
+            this.dataGridView3.TabIndex = 53;
+            // 
+            // lbMonthlyLedgerChart
+            // 
+            this.lbMonthlyLedgerChart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbMonthlyLedgerChart.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbMonthlyLedgerChart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbMonthlyLedgerChart.Location = new System.Drawing.Point(443, 323);
+            this.lbMonthlyLedgerChart.Name = "lbMonthlyLedgerChart";
+            this.lbMonthlyLedgerChart.Size = new System.Drawing.Size(232, 23);
+            this.lbMonthlyLedgerChart.TabIndex = 58;
+            this.lbMonthlyLedgerChart.Text = "Monthly Ledger Chart";
+            // 
+            // lbPetReportChart
+            // 
+            this.lbPetReportChart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbPetReportChart.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbPetReportChart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbPetReportChart.Location = new System.Drawing.Point(865, 323);
+            this.lbPetReportChart.Name = "lbPetReportChart";
+            this.lbPetReportChart.Size = new System.Drawing.Size(232, 23);
+            this.lbPetReportChart.TabIndex = 59;
+            this.lbPetReportChart.Text = "Pet Report Chart";
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(443, 349);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowTemplate.Height = 25;
+            this.dataGridView4.Size = new System.Drawing.Size(388, 236);
+            this.dataGridView4.TabIndex = 60;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(865, 349);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowTemplate.Height = 25;
+            this.dataGridView5.Size = new System.Drawing.Size(388, 236);
+            this.dataGridView5.TabIndex = 61;
             // 
             // StoreManagementForm
             // 
@@ -153,12 +322,26 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(1272, 642);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.lbPetReportChart);
+            this.Controls.Add(this.lbMonthlyLedgerChart);
+            this.Controls.Add(this.lbEmployees);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.lbPetReport);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.lbMonthlyLedger);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbInventory);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grvInventory);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbOrders);
             this.Controls.Add(this.logo);
@@ -166,9 +349,14 @@
             this.Name = "StoreManagementForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fur Ever Friends Pet Shop - Store Management";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvInventory)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -180,9 +368,23 @@
         private Button btnDelete;
         private Button btnAdd;
         private Button btnCancel;
-        private DataGridView dataGridView1;
+        private DataGridView grvInventory;
         private PictureBox pictureBox1;
         private Label lbOrders;
         private PictureBox logo;
+        private Label lbInventory;
+        private Label lbMonthlyLedger;
+        private DataGridView dataGridView1;
+        private Label lbPetReport;
+        private DataGridView dataGridView2;
+        private Label lbEmployees;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView dataGridView3;
+        private Label lbMonthlyLedgerChart;
+        private Label lbPetReportChart;
+        private DataGridView dataGridView4;
+        private DataGridView dataGridView5;
     }
 }
