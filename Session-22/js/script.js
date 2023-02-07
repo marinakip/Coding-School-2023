6688afdba89b4a515da58f1dfc241b3c1ab65a2c
marinakip <marinakipourou@hotmail.com>
2023-02-07 14:24:22 +0200

    Add Exercise 2: Palindrome Check
    Add elements in html for second exercise, created  checkPalindrome() and small changes to reverse()

--- Session-22/js/script.js
+++ Session-22/js/script.js
@@ -1,8 +1,23 @@
-function reverse() {
-    let input = document.getElementById("input-reverse").value;
+
+function reverse(input) {
     let splittedInput = input.split("");
     let reversedArray = splittedInput.reverse();
     let result = reversedArray.join("");
-    document.getElementById("output-reverse").value = result;
+    return result;
+}
 
+function reverseInput() {
+    let inputString = document.getElementById("input-reverse").value;
+    let reversedInput = reverse(inputString);
+    document.getElementById("output-reverse").value = reversedInput;
+}
+
+function checkPalindrome() {
+    let inputPalindrome = document.getElementById("input-palindrome").value;
+    let reversedString = reverse(inputPalindrome);
+    if (inputPalindrome == reversedString) {
+        document.getElementById("output-palindrome").value = "It is palindrome"
+    } else {
+        document.getElementById("output-palindrome").value = "It is not palindrome"
+    }
 }
