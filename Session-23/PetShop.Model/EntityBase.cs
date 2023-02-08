22a5c4d32c59c959d1154aea47d80913ffbb3c08
marinakip <marinakipourou@hotmail.com>
2023-02-08 14:02:29 +0200

    Add EntityBase and IEntityBase in model

---
+++ Session-23/PetShop.Model/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Model {
+    public abstract class EntityBase : IEntityBase {
+        public int Id { get; set; }
+    }
+}
