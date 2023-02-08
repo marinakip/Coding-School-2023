be23edab1f4048d8a7b491132e46428e14e2dd21
marinakip <marinakipourou@hotmail.com>
2023-02-08 14:48:40 +0200

    Add CustomerRepository implementation

--- Session-23/PetShop.EF/Repositories/CustomerRepository.cs
+++ Session-23/PetShop.EF/Repositories/CustomerRepository.cs
@@ -1,4 +1,6 @@
-﻿using PetShop.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.EF.Context;
+using PetShop.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,25 +9,42 @@ using System.Threading.Tasks;
 
 namespace PetShop.EF.Repositories {
     public class CustomerRepository : IEntityRepository<Customer> {
-        //TODO: Implement Customer repository
         public void Add(Customer entity) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
         }
 
         public void Delete(int id) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            var dbCustomer = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
+            if (dbCustomer is null)
+                return;
+            context.Remove(dbCustomer);
+            context.SaveChanges();
         }
 
         public IList<Customer> GetAll() {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            return context.Customers.Include(customer => customer.Transactions).ToList();
         }
 
         public Customer? GetById(int id) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            return context.Customers.Where(customer => customer.Id == id)
+                .Include(customer => customer.Transactions).SingleOrDefault();
         }
 
         public void Update(int id, Customer entity) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            var dbCustomer = context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
+            if (dbCustomer is null)
+                return;
+            dbCustomer.Name = entity.Name;
+            dbCustomer.Surname = entity.Surname;
+            dbCustomer.Phone = entity.Phone;    
+            dbCustomer.Tin = entity.Tin;
+            context.SaveChanges();
         }
     }
 }
