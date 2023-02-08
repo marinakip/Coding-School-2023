035e15c1f3c831ce6707f3c364ecccf9e65296f9
marinakip <marinakipourou@hotmail.com>
2023-02-08 13:54:02 +0200

    Add repositories folder with repository classes (not yet implemented)

---
+++ Session-23/PetShop.EF/Repositories/PetRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.EF.Repositories {
+    public class PetRepository {
+        //TODO: Implement Pet repository
+    }
+}
---
+++ Session-23/PetShop.EF/Repositories/TransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.EF.Repositories {
+    public class TransactionRepository {
+        //TODO: Implement Transaction repository
+    }
+}
