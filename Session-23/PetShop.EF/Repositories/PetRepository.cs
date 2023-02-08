35158ff0a64a5fe4ff6371decd80cdd12c72f171
marinakip <marinakipourou@hotmail.com>
2023-02-08 15:12:09 +0200

    Add PetRepository implementation

--- Session-23/PetShop.EF/Repositories/PetRepository.cs
+++ Session-23/PetShop.EF/Repositories/PetRepository.cs
@@ -1,4 +1,6 @@
-﻿using PetShop.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.EF.Context;
+using PetShop.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,25 +9,44 @@ using System.Threading.Tasks;
 
 namespace PetShop.EF.Repositories {
     public class PetRepository : IEntityRepository<Pet> {
-        //TODO: Implement Pet repository
+        
         public void Add(Pet entity) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
         }
 
         public void Delete(int id) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            var dbPet = context.Pets.Where(pet => pet.Id == id).SingleOrDefault();
+            if (dbPet is null)
+                return;
+            context.Remove(dbPet);
+            context.SaveChanges();
         }
 
         public IList<Pet> GetAll() {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            return context.Pets.Include(pet => pet.Transactions).ToList();
         }
 
         public Pet? GetById(int id) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            return context.Pets.Where(pet => pet.Id == id)
+                .Include(pet => pet.Transactions).SingleOrDefault();
         }
 
         public void Update(int id, Pet entity) {
-            throw new NotImplementedException();
+            using var context = new PetShopDbContext();
+            var dbPet = context.Pets.Where(pet => pet.Id == id).SingleOrDefault();
+            if (dbPet is null)
+                return;
+            dbPet.Breed = entity.Breed;
+            dbPet.AnimalType= entity.AnimalType;  
+            dbPet.PetStatus = entity.PetStatus; 
+            dbPet.Price = entity.Price;
+            dbPet.Cost = entity.Cost;
+            context.SaveChanges();
         }
     }
 }
