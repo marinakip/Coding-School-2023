ee32a72ece03f095e34fe953d61572573c9d942f
marinakip <marinakipourou@hotmail.com>
2023-02-10 03:00:28 +0200

    Add CustomersController, Customers View and Customers in the navigation bar (only Index implemented)
    Add CustomersController (only Index implemented), View for customers(only for Index) and added customers to Layout in the navigation bar

---
+++ Session-23/PetShop.Web.Mvc/Controllers/CustomersController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.EF.Repositories;
+using PetShop.Model;
+
+namespace PetShop.Web.Mvc.Controllers {
+    public class CustomersController : Controller {
+        private readonly IEntityRepository<Customer> _customerRepository;
+
+        public CustomersController(IEntityRepository<Customer> customerRepository) {
+            _customerRepository = customerRepository;
+        }
+
+        // GET: CustomersController
+        public ActionResult Index() {
+            var customers = _customerRepository.GetAll();   
+            return View(model: customers);
+        }
+
+        // GET: CustomersController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: CustomersController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: CustomersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: CustomersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: CustomersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            } catch {
+                return View();
+            }
+        }
+    }
+}
