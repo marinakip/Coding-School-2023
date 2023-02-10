fd2a4f92539a44c014febeef46b1497758e68a60
marinakip <marinakipourou@hotmail.com>
2023-02-10 03:27:52 +0200

    Add EmployeesController (implementation for Index only), View Employees(only Index) and added in Layout

---
+++ Session-23/PetShop.Web.Mvc/Controllers/EmployeesController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.EF.Repositories;
+using PetShop.Model;
+
+namespace PetShop.Web.Mvc.Controllers {
+    public class EmployeesController : Controller {
+        private readonly IEntityRepository<Employee> _employeeRepository;
+        public EmployeesController(IEntityRepository<Employee> employeeRepository) {
+            _employeeRepository = employeeRepository;
+        }
+
+        // GET: EmployeesController
+        public ActionResult Index() {
+            var employees = _employeeRepository.GetAll();
+            return View(model: employees);
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            } catch {
+                return View();
+            }
+        }
+    }
+}
