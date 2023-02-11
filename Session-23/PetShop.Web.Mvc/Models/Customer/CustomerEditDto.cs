fdcafae30dfad34b7b5d234a8ec98b937f9413f3
marinakip <marinakipourou@hotmail.com>
2023-02-11 04:10:00 +0200

    Added and implemeted CustomerDeleteDto,  CustomerDetailsDto and CustomerEditDto

---
+++ Session-23/PetShop.Web.Mvc/Models/Customer/CustomerEditDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetShop.Web.Mvc.Models.Customer {
+    public class CustomerEditDto {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "You have exceeded the limit of 50 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(100, ErrorMessage = "You have exceeded the limit of 100 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Surname is required")]
+        public string Surname { get; set; } = null!;
+
+        [MaxLength(15, ErrorMessage = "You have exceeded the limit of 15 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Phone is required")]
+        public string Phone { get; set; } = null!;
+
+        [MaxLength(30, ErrorMessage = "You have exceeded the limit of 30 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Tax Identification Number is required")]
+        [Display(Name = "Tax Identification Number")]
+        public string Tin { get; set; } = null!;
+    }
+}
