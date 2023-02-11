fb65d5cd4e6e6467a744d4716f6b60cbd3b4ecce
marinakip <marinakipourou@hotmail.com>
2023-02-11 03:15:21 +0200

    Implemented CustomerCreateDto

--- Session-23/PetShop.Web.Mvc/Models/Customer/CustomerCreateDto.cs
+++ Session-23/PetShop.Web.Mvc/Models/Customer/CustomerCreateDto.cs
@@ -1,4 +1,23 @@
-﻿namespace PetShop.Web.Mvc.Models.Customer {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetShop.Web.Mvc.Models.Customer {
     public class CustomerCreateDto {
+
+        [MaxLength(50, ErrorMessage = "You have exceeded the limit of 50 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(100, ErrorMessage = "You have exceeded the limit of 100 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Surname is required")]
+        public string Surname { get; set; } = null!;
+
+        [MaxLength(15, ErrorMessage = "You have exceeded the limit of 15 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Phone is required")]
+        public string Phone { get; set; } = null!;
+
+        [MaxLength(30, ErrorMessage = "You have exceeded the limit of 30 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Tax Identification Number is required")]
+        [Display(Name = "Tax Identification Number")]
+        public string Tin { get; set; } = null!;
     }
 }
