cf50770408759cc07753670ae179f83906783b60
marinakip <marinakipourou@hotmail.com>
2023-02-12 02:17:20 +0200

    Add Monthly Ledger DTO and Month enum in Mvc.Models

--- Session-23/PetShop.Web.Mvc/Models/MonthlyLedger/MonthlyLedgerDto.cs
+++ Session-23/PetShop.Web.Mvc/Models/MonthlyLedger/MonthlyLedgerDto.cs
@@ -1,6 +1,27 @@
-﻿namespace PetShop.Web.Mvc.Models.MonthlyLedger {
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetShop.Web.Mvc.Models.MonthlyLedger {
     public class MonthlyLedgerDto {
 
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public Month Month { get; set; }
+
+        [Precision(6, 2)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Income is required")]
+        public decimal Income { get; set; }
+
+        [Precision(6, 2)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Income is required")]
+        public decimal Expenses { get; set; }
+
+        [Display(Name ="Total Profit")]
+        [Precision(6, 2)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Income is required")]
+        public decimal Total { get; set; }
 
     }
 }
