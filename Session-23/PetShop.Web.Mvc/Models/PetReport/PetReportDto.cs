0ef65ab19dec81cd2328a7b727e637cad9a221f7
marinakip <marinakipourou@hotmail.com>
2023-02-12 02:31:17 +0200

    Add PetReportDto in Mvc.Models

--- Session-23/PetShop.Web.Mvc/Models/PetReport/PetReportDto.cs
+++ Session-23/PetShop.Web.Mvc/Models/PetReport/PetReportDto.cs
@@ -1,4 +1,22 @@
-﻿namespace PetShop.Web.Mvc.Models.PetReport {
+﻿using PetShop.Model.Enums;
+using PetShop.Web.Mvc.Models.MonthlyLedger;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetShop.Web.Mvc.Models.PetReport {
     public class PetReportDto {
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Year is required")]
+        public int Year { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Month is required")]
+        public Month Month { get; set; }
+
+        [Display(Name = "Animal Type")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Animal type is required")]
+        public AnimalType TypeAnimal { get; set; }
+
+        [Display(Name = "Total Sold")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Total Sold is required")]
+        public int TotalSold { get; set; }
     }
 }
