9f9573adde2116f25d4791c7c19722f63d1054e7
marinakip <marinakipourou@hotmail.com>
2023-02-12 02:20:05 +0200

    Small changes in ErrorMessages

--- Session-23/PetShop.Web.Mvc/Models/MonthlyLedger/MonthlyLedgerDto.cs
+++ Session-23/PetShop.Web.Mvc/Models/MonthlyLedger/MonthlyLedgerDto.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 namespace PetShop.Web.Mvc.Models.MonthlyLedger {
     public class MonthlyLedgerDto {
 
-        [Required]
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Year is required")]
         public int Year { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Month is required")]
         public Month Month { get; set; }
 
         [Precision(6, 2)]
@@ -15,12 +16,12 @@ namespace PetShop.Web.Mvc.Models.MonthlyLedger {
         public decimal Income { get; set; }
 
         [Precision(6, 2)]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Income is required")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Expenses is required")]
         public decimal Expenses { get; set; }
 
         [Display(Name ="Total Profit")]
         [Precision(6, 2)]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Income is required")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Total Profit is required")]
         public decimal Total { get; set; }
 
     }
