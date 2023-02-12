7fb2e4814fcec06ba53c66a417e446c4de39f5b2
marinakip <marinakipourou@hotmail.com>
2023-02-12 04:59:10 +0200

    Added all Views for PetFood and completed PetFoodsController
    Add all Views for PetFood and completed PetFoodsController
    Note - Issue: In Create it only takes decimal numbers with "," not "."

--- Session-23/PetShop.Web.Mvc/Controllers/PetFoodsController.cs
+++ Session-23/PetShop.Web.Mvc/Controllers/PetFoodsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using PetShop.EF.Repositories;
 using PetShop.Model;
+using PetShop.Web.Mvc.Models.Employee;
+using PetShop.Web.Mvc.Models.PetFood;
 
 namespace PetShop.Web.Mvc.Controllers {
     public class PetFoodsController : Controller {
@@ -19,7 +21,23 @@ namespace PetShop.Web.Mvc.Controllers {
 
         // GET: PetFoodsController/Details/5
         public ActionResult Details(int id) {
-            return View();
+            if (id == null) {
+                return NotFound();
+            }
+
+            var petFood = _petFoodRepository.GetById(id);
+            if (petFood == null) {
+                return NotFound();
+            }
+
+            var viewPetFood = new PetFoodDetailsDto {
+                Id = petFood.Id,
+                AnimalType = petFood.AnimalType,
+                Price = petFood.Price,
+                Cost = petFood.Cost,
+                Transactions = petFood.Transactions.ToList()
+            };
+            return View(model: viewPetFood);
         }
 
         // GET: PetFoodsController/Create
@@ -30,9 +48,15 @@ namespace PetShop.Web.Mvc.Controllers {
         // POST: PetFoodsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection) {
+        public ActionResult Create(PetFoodCreateDto petFood) {
             try {
-                return RedirectToAction(nameof(Index));
+                if (!ModelState.IsValid) {
+                    return View();
+                }
+
+                var dbPetFood = new PetFood(petFood.AnimalType, petFood.Price, petFood.Cost);
+                _petFoodRepository.Add(dbPetFood);
+                return RedirectToAction("Index");
             } catch {
                 return View();
             }
@@ -40,23 +64,56 @@ namespace PetShop.Web.Mvc.Controllers {
 
         // GET: PetFoodsController/Edit/5
         public ActionResult Edit(int id) {
-            return View();
+            
+            var dbPetFood = _petFoodRepository.GetById(id);
+            if (dbPetFood == null) {
+                return NotFound();
+            }
+
+            var viewPetFood = new PetFoodEditDto {
+                Id = dbPetFood.Id,
+                AnimalType = dbPetFood.AnimalType,
+                Price = dbPetFood.Price,
+                Cost = dbPetFood.Cost
+            };
+            return View(model: viewPetFood);
+           
         }
 
         // POST: PetFoodsController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection) {
-            try {
-                return RedirectToAction(nameof(Index));
-            } catch {
+        public ActionResult Edit(int id, PetFoodEditDto petFood) {
+            if (!ModelState.IsValid) {
                 return View();
             }
+
+            var dbPetFood = _petFoodRepository.GetById(id);
+            if (dbPetFood == null) {
+                return NotFound();
+            }
+
+            dbPetFood.AnimalType = petFood.AnimalType;
+            dbPetFood.Price = petFood.Price;
+            dbPetFood.Cost = petFood.Cost;
+
+            _petFoodRepository.Update(id, dbPetFood);
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: PetFoodsController/Delete/5
         public ActionResult Delete(int id) {
-            return View();
+            var dbPetFood = _petFoodRepository.GetById(id);
+            if (dbPetFood == null) {
+                return NotFound();
+            }
+            var viewPetFood = new PetFoodDeleteDto {
+                Id = dbPetFood.Id,
+                AnimalType = dbPetFood.AnimalType,
+                Price = dbPetFood.Price,
+                Cost = dbPetFood.Cost
+            };
+            return View(model: viewPetFood);
         }
 
         // POST: PetFoodsController/Delete/5
@@ -64,6 +121,7 @@ namespace PetShop.Web.Mvc.Controllers {
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection) {
             try {
+                _petFoodRepository.Delete(id);
                 return RedirectToAction(nameof(Index));
             } catch {
                 return View();
