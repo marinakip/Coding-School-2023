f6d39ab31b803c4fef343772cce14cde8831449d
marinakip <marinakipourou@hotmail.com>
2023-02-14 00:58:01 +0200

    Add CartState and button buy ( Step 3a blazorschool tutorial)
    Add CartState to Sates folder in client and button buy  and property DisplayBuyButton to ProductDisplay.razor. Then add property DisplayBuyButton when we use ProductDisplay in ProductList. In that way we create a cart service using state and then allow the user to add products to the cart when the buy button is clicked.
    (Step 3a: BlazorSchool.com tutorial Wasm)

---
+++ Session-26/Session-26/Client/States/CartState.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using Session_26.Shared;
+using System.Net.Http.Json;
+
+namespace Session_26.Client.States
+{
+// Tutorial Description of this class
+//This cart service will hold the information about the product which is selected by the user.
+//This is also known as storing the website's state. the website's state is stored in the memory.
+    public class CartState {
+        private readonly HttpClient _httpClient;
+        public List<Product> SelectedItems { get; set; } = new();
+
+        public CartState(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddProductToCartAsync(int productId) {
+            if(SelectedItems.Any(product => product.Id == productId) is false) {
+                var products = await _httpClient.GetFromJsonAsync<List<Product>>("sample-data/products.json") ?? new();
+                var product = products.First(product => product.Id == productId);
+                SelectedItems.Add(product);
+            }
+        }
+    }
+}
--- Session-26/Session-26/Server/Program.cs
+++ Session-26/Session-26/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Session_26.Client.States;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<CartState>();
 
 var app = builder.Build();
 
