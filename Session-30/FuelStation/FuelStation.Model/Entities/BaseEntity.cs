c673e7f9969df27877f6baa2d24f106616ce03d7
marinakip <marinakipourou@hotmail.com>
2023-02-19 20:18:38 +0200

    Add IEntityBase and BaseEntity in Models

---
+++ Session-30/FuelStation/FuelStation.Model/Entities/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Model.Entities {
+    /// <summary>
+    /// It is the class that all entities in Fuel Station inherit from.
+    /// It implements IEntityBase interface.
+    /// </summary>
+    public class BaseEntity : IEntityBase {
+
+        #region Properties  
+        /// <summary>
+        /// Gets or sets the global unique identifier
+        /// </summary>
+        public Guid Id { get; set; }
+        #endregion Properties
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the BaseEntity class
+        /// </summary>
+        public BaseEntity() {
+            Id = Guid.NewGuid();
+        }
+        #endregion Constructor
+    }
+}
