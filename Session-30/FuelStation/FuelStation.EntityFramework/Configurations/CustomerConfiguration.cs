e27bf1299af38665a05ef0eb75bc52a1dbae9064
marinakip <marinakipourou@hotmail.com>
2023-02-20 04:24:43 +0200

    Add CustomerConfiguration in EntityFramework

---
+++ Session-30/FuelStation/FuelStation.EntityFramework/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using FuelStation.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EntityFramework.Configurations {
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer> {
+        public void Configure(EntityTypeBuilder<Customer> builder) {
+            // Table Name
+            builder.ToTable("Customers");
+
+            // Primary Key
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(t => t.Name).HasMaxLength(50).IsRequired();
+            builder.Property(t => t.Surname).HasMaxLength(100).IsRequired();
+            builder.HasIndex(t => t.CardNumber).IsUnique();
+
+            // Relations
+        }
+    }
+}
