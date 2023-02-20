62d5f919daa9e60836777b264fbcbcb5697c95a4
marinakip <marinakipourou@hotmail.com>
2023-02-20 04:52:29 +0200

    Add FuelStationDbContext in EntityFramework

---
+++ Session-30/FuelStation/FuelStation.EntityFramework/Context/FuelStationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using FuelStation.EntityFramework.Configurations;
+using FuelStation.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EntityFramework.Context {
+    public class FuelStationDbContext : DbContext {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Item> Items { get; set; } = null!;
+        public DbSet<Transaction> Transactions { get; set; } = null!;
+        public DbSet<TransactionLine> TransactionLines { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());            
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            //modelBuilder.ApplyConfiguration(new ItemConfiguration());             //TODO: Uncomment them when done with configs
+            //modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            //modelBuilder.ApplyConfiguration(new TransactionLineConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            // If it is not connected to database, use TrustServerCertificate=False; I had to change it to True to connect to my database
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FuelStation;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
