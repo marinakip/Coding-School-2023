238d5ff40c00f599b800ee51da935199ccfb6ca0
marinakip <marinakipourou@hotmail.com>
2023-02-20 04:46:53 +0200

    Add Employee configuration in EntityFramework

---
+++ Session-30/FuelStation/FuelStation.EntityFramework/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using FuelStation.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EntityFramework.Configurations {
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee> {
+        public void Configure(EntityTypeBuilder<Employee> builder) {
+            // Table Name
+            builder.ToTable("Employees");
+
+            // Primary Key
+            builder.HasKey(employee => employee.Id);
+            builder.Property(employee => employee.Id).ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(employee => employee.Name).HasMaxLength(50).IsRequired();
+            builder.Property(employee => employee.Surname).HasMaxLength(100).IsRequired();
+            builder.Property(employee => employee.HireDateStart).IsRequired();
+            builder.Property(employee => employee.HireDateEnd);
+            builder.Property(employee => employee.SallaryPerMonth).HasPrecision(7,2).IsRequired();
+            builder.Property(employee => employee.EmployeeType).IsRequired();
+
+            // Relations
+        }
+    }
+}
