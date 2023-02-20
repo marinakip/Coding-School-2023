ea1b9d0ee285c4e7231c8e025c9fea4da74ec9ec
marinakip <marinakipourou@hotmail.com>
2023-02-20 04:32:46 +0200

    Small fix in parameter names

--- Session-30/FuelStation/FuelStation.EntityFramework/Configurations/CustomerConfiguration.cs
+++ Session-30/FuelStation/FuelStation.EntityFramework/Configurations/CustomerConfiguration.cs
@@ -14,13 +14,13 @@ namespace FuelStation.EntityFramework.Configurations {
             builder.ToTable("Customers");
 
             // Primary Key
-            builder.HasKey(t => t.Id);
-            builder.Property(t => t.Id).ValueGeneratedOnAdd();
+            builder.HasKey(customer => customer.Id);
+            builder.Property(customer => customer.Id).ValueGeneratedOnAdd();
 
             // Properties
-            builder.Property(t => t.Name).HasMaxLength(50).IsRequired();
-            builder.Property(t => t.Surname).HasMaxLength(100).IsRequired();
-            builder.HasIndex(t => t.CardNumber).IsUnique();
+            builder.Property(customer => customer.Name).HasMaxLength(50).IsRequired();
+            builder.Property(customer => customer.Surname).HasMaxLength(100).IsRequired();
+            builder.HasIndex(customer => customer.CardNumber).IsUnique();
 
             // Relations
         }
