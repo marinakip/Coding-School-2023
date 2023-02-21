540c079e40a61d81523931bb75339ecfa1dc3c9d
marinakip <marinakipourou@hotmail.com>
2023-02-21 11:10:35 +0200

    Add Item's Code generation in Model.Item
    Item's Code is generated in a string that represents a barcode, using EAN-13 barcode algorithm

--- Session-30/FuelStation/FuelStation.Model/Entities/Item.cs
+++ Session-30/FuelStation/FuelStation.Model/Entities/Item.cs
@@ -1,7 +1,9 @@
 ﻿using FuelStation.Model.Enumerations;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -51,8 +53,8 @@ namespace FuelStation.Model.Entities {
         /// refers to the amount of money that the buyer is going to spend.</param>
         /// <param name="cost">The item's cost. Nptice that cost is different from <value>Price</value> and it refers to 
         /// the original amount of money spent by the store, not the buyer.</param>
-        public Item(string code, string description, ItemType itemType, decimal price, decimal cost) : base() {
-            Code = code;    
+        public Item(string description, ItemType itemType, decimal price, decimal cost) : base() {
+            Code = GenerateItemCode();    
             Description = description;
             ItemType = itemType;
             Price = price;
@@ -61,6 +63,45 @@ namespace FuelStation.Model.Entities {
 
         }
         #endregion Constructor
+
+
+        #region Help Methods
+        /// <summary>
+        /// Generates Item code using EAN-13 algorithm for barcodes
+        /// </summary>    
+        private string? GenerateItemCode() {
+            string country = "520"; // Greece
+            string manufacturer = "9876"; // our fuel station code goes here, this is an example
+            string product = GenerateProductCode(); // should be 5 digits 
+            string temp = $"{country}{manufacturer}{product}";
+            int sum = 0;
+            int digit = 0;
+            
+            // Checksum digit calculation
+            for (int i = temp.Length; i >= 1; i--) {
+               
+                digit = Convert.ToInt32(temp.Substring(i - 1, 1));                
+                               
+                if (i % 2 == 0) { // odd  
+                    sum += digit * 3;
+                } else { // even
+                    sum += digit * 1;
+                }
+            }
+            int checkSum = (10 - (sum % 10)) % 10;
+            return $"{temp}{checkSum}";
+        }
+
+        /// <summary>
+        /// It generates a random 5 digit code for our product code
+        /// </summary>        
+        private string GenerateProductCode() {
+           Random random = new Random();
+            return $"{random.Next(10000, 99999)}";
+        }
+        #endregion Help Methods
+
+
 
         #region Relations
         // One to many relationship, one item has many transaction lines
