19f309d44bd694126fcb77af80a6c833350c19a3
marinakip <marinakipourou@hotmail.com>
2023-02-21 12:57:21 +0200

    Add EmployeeRepository

---
+++ Session-30/FuelStation/FuelStation.EntityFramework/Repositories/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using FuelStation.EntityFramework.Context;
+using FuelStation.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EntityFramework.Repositories {
+    public class EmployeeRepository : IEntityRepository<Employee> {
+        public void Add(Employee entity) {
+            using var context = new FuelStationDbContext();
+            if (entity.Id != Guid.Empty) {
+                throw new ArgumentException("Given entity should not have Id set", nameof(entity));
+            }
+            context.Employees.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(Guid id) {
+            using var context = new FuelStationDbContext();
+            var dbEmployee = context.Employees.SingleOrDefault(Employee => Employee.Id == id);
+            if (dbEmployee is null) {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            context.Employees.Remove(dbEmployee);
+            context.SaveChanges();
+        }
+
+        public IList<Employee> GetAll() {
+            using var context = new FuelStationDbContext();
+            return context.Employees.ToList();
+        }
+
+        public Employee? GetById(Guid id) {
+            using var context = new FuelStationDbContext();
+            return context.Employees.SingleOrDefault(Employee => Employee.Id == id);
+        }
+
+        public void Update(Guid id, Employee entity) {
+            using var context = new FuelStationDbContext();
+            var dbEmployee = context.Employees.SingleOrDefault(Employee => Employee.Id == id);
+            if (dbEmployee is null) {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            dbEmployee.Name = entity.Name;
+            dbEmployee.Surname = entity.Surname;
+            dbEmployee.HireDateStart = entity.HireDateStart;
+            dbEmployee.HireDateEnd = entity.HireDateEnd;
+            dbEmployee.SallaryPerMonth = entity.SallaryPerMonth;    
+            dbEmployee.EmployeeType = entity.EmployeeType;  
+            context.SaveChanges();
+        }
+    }
+}
