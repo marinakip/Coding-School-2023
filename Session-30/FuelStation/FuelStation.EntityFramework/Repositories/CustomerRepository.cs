2c0e0268c709413f36991f664a6dd6ea542ed66a
marinakip <marinakipourou@hotmail.com>
2023-02-21 12:40:07 +0200

    Add IEntityRepository and CustomerRepository

---
+++ Session-30/FuelStation/FuelStation.EntityFramework/Repositories/CustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using FuelStation.EntityFramework.Context;
+using FuelStation.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EntityFramework.Repositories {
+    public class CustomerRepository : IEntityRepository<Customer> {
+        public void Add(Customer entity) {
+            using var context = new FuelStationDbContext();
+            if (entity.Id != null) {
+                throw new ArgumentException("Given entity should not have Id set", nameof(entity));
+            }
+            context.Customers.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(Guid id) {
+            using var context = new FuelStationDbContext();
+            var dbCustomer = context.Customers.SingleOrDefault(Customer => Customer.Id == id);
+            if (dbCustomer is null) {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            context.Customers.Remove(dbCustomer);
+            context.SaveChanges();
+        }
+
+        public IList<Customer> GetAll() {
+            using var context = new FuelStationDbContext();
+            return context.Customers.ToList();
+        }
+
+        public Customer? GetById(Guid id) {
+            using var context = new FuelStationDbContext();
+            return context.Customers.SingleOrDefault(Customer => Customer.Id == id);
+        }
+
+        public void Update(Guid id, Customer entity) {
+            using var context = new FuelStationDbContext();
+            var dbCustomer = context.Customers.SingleOrDefault(Customer => Customer.Id == id);
+            if (dbCustomer is null) {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            dbCustomer.Name = entity.Name;
+            dbCustomer.Surname = entity.Surname;            
+            // Card Number is auto generated
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Session-30/FuelStation/FuelStation.EntityFramework/Repositories/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using FuelStation.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EntityFramework.Repositories {
+    public interface IEntityRepository<TEntity> where TEntity : BaseEntity {
+        IList<TEntity> GetAll();
+        TEntity? GetById(Guid id);
+        void Add(TEntity entity);
+        void Update(Guid id, TEntity entity);
+        void Delete(Guid id);
+    }
+}
