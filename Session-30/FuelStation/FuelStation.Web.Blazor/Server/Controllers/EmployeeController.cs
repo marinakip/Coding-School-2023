154ddcb0392757d7b8ec48b99fc93ad352851f5f
marinakip <marinakipourou@hotmail.com>
2023-02-23 04:12:38 +0200

    Add EmployeeController in Server and EmployeeCreateDto in Shared

---
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,86 @@
+﻿using FuelStation.EntityFramework.Repositories;
+using FuelStation.Model.Entities;
+using FuelStation.Web.Blazor.Shared.EmployeeDataTranferObjects;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using Microsoft.AspNetCore.Http;
+using FuelStation.Web.Blazor.Shared.CustomerDataTransferObjects;
+using FuelStation.Web.Blazor.Client.Pages.Customer;
+
+namespace FuelStation.Web.Blazor.Server.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase {
+
+        private readonly IEntityRepository<Employee> _employeeRepository;
+
+        public EmployeeController(IEntityRepository<Employee> employeeRepository) {
+            _employeeRepository = employeeRepository;
+        }
+
+        // GET: <EmployeeController>
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeListDto>> Get() {
+            var result = _employeeRepository.GetAll();
+            return result.Select(employee => new EmployeeListDto {
+                Id = employee.Id,
+                Name = employee.Name,
+                Surname = employee.Surname,
+                HireDateStart = employee.HireDateStart,
+                HireDateEnd = employee.HireDateEnd,
+                SallaryPerMonth = employee.SallaryPerMonth,
+                EmployeeType = employee.EmployeeType
+            });
+        }
+
+        // GET <EmployeeController>/450F87A0-9FC8-4C0E-BA65-B45EEFEB9B12
+        [HttpGet("{id}")]
+        public async Task<EmployeeEditDto> GetById(Guid id) {
+            var result = _employeeRepository.GetById(id);
+            return new EmployeeEditDto {
+                Id = id,
+                Name = result.Name,
+                Surname = result.Surname,
+                HireDateStart = result.HireDateStart,
+                HireDateEnd = result.HireDateEnd,
+                SallaryPerMonth = result.SallaryPerMonth,
+                EmployeeType = result.EmployeeType
+            };
+        }
+
+        // POST <EmployeeController>
+        [HttpPost]
+        public async Task Post(EmployeeCreateDto employee) {
+            var newEmployee = new Employee(
+                employee.Name,
+                employee.Surname,
+                //DateTime.Now,
+                (DateTime)employee.HireDateStart,
+                (DateTime)employee.HireDateEnd,
+                employee.SallaryPerMonth,
+                employee.EmployeeType
+            );
+            _employeeRepository.Add(newEmployee);
+        }
+
+        // PUT <EmployeeController>/450F87A0-9FC8-4C0E-BA65-B45EEFEB9B12
+        [HttpPut]
+        public async Task Put(EmployeeEditDto employee) {
+            var itemToUpdate = _employeeRepository.GetById(employee.Id);
+            itemToUpdate.Name = employee.Name;
+            itemToUpdate.Surname = employee.Surname;
+            itemToUpdate.HireDateStart = (DateTime) employee.HireDateStart;
+            itemToUpdate.HireDateEnd = (DateTime) employee.HireDateEnd;
+            itemToUpdate.SallaryPerMonth = employee.SallaryPerMonth;
+            itemToUpdate.EmployeeType = employee.EmployeeType;
+            _employeeRepository.Update(employee.Id, itemToUpdate);
+        }
+
+        // DELETE <EmployeeController>/450F87A0-9FC8-4C0E-BA65-B45EEFEB9B12
+        [HttpDelete("{id}")]
+        public void Delete(Guid id) {
+            _employeeRepository.Delete(id);
+        }
+    }
+}
---
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Shared/EmployeeDataTranferObjects/EmployeeCreateDto.cs
@@ -0,0 +1,37 @@
+﻿using FuelStation.Model.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FuelStation.Web.Blazor.Shared.EmployeeDataTranferObjects {
+    public class EmployeeCreateDto {
+        [MaxLength(50, ErrorMessage = "You have exceeded the limit of 50 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(100, ErrorMessage = "You have exceeded the limit of 100 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Surname is required")]
+        public string Surname { get; set; } = null!;
+
+        [Display(Name = "Hire Date Start")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Hire Date Start is required")]
+        public DateTime? HireDateStart { get; set; }
+
+        [Display(Name = "Hire Date End")]
+        public DateTime? HireDateEnd { get; set; }
+
+        [Display(Name = "Sallary Per Month")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Sallary Per Month is required")]
+        [RegularExpression(@"^\d{0,5}(\.\d{1,2})?$", ErrorMessage = "Please enter a number for Sallary Per Month with up to 2 decimal places and up to 7 total digits.")]
+        public decimal SallaryPerMonth { get; set; }
+
+        [Display(Name = "Employee Type")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Employee Type is required")]
+        public EmployeeType EmployeeType { get; set; }
+
+    }
+}
