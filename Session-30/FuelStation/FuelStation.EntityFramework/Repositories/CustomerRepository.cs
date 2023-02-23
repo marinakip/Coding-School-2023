b60ef91d455ea1d31ff68d795b12eaf41c1ce136
marinakip <marinakipourou@hotmail.com>
2023-02-23 02:58:57 +0200

    Add method to check if card number exists before add customer to database
    Add method to check if card number exists before add customer to database. Also, uncommented style for customers page and deleted commented lines

--- Session-30/FuelStation/FuelStation.EntityFramework/Repositories/CustomerRepository.cs
+++ Session-30/FuelStation/FuelStation.EntityFramework/Repositories/CustomerRepository.cs
@@ -10,8 +10,7 @@ using System.Threading.Tasks;
 namespace FuelStation.EntityFramework.Repositories {
     public class CustomerRepository : IEntityRepository<Customer> {
         public void Add(Customer entity) {
-            using var context = new FuelStationDbContext();
-            //if (entity.Id != Guid.Empty || entity.Id != null) {
+            using var context = new FuelStationDbContext();            
             if (entity.Id != default(Guid)) {
                 throw new ArgumentException("Given entity should not have Id set", nameof(entity));
             }
@@ -19,8 +18,24 @@ namespace FuelStation.EntityFramework.Repositories {
             // since if someone creates a customer with default guid, a generated card number and a name and surname it will be added.
             // Maybe a more secure way it would be to create a list of predefined guids and card numbers and get them from there, to
             // create a new customer.
-            context.Customers.Add(entity);
-            context.SaveChanges();
+
+            if (CardNumberExists(entity)) {
+                throw new Exception($"Customer with Card Number {entity.CardNumber} already exists");
+            } else {
+                context.Customers.Add(entity);
+                context.SaveChanges();
+            }           
+        }
+
+        private bool CardNumberExists(Customer entity) {
+            using var context = new FuelStationDbContext();
+            var dbCustomer = context.Customers
+               .Where(customer => customer.CardNumber == entity.CardNumber).FirstOrDefault();
+            if (dbCustomer != null) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
         public void Delete(Guid id) {
