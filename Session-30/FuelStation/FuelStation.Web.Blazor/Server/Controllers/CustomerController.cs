17ac4697fbd3f2dc2e7c7ae4e3105343ac0dbc1f
marinakip <marinakipourou@hotmail.com>
2023-02-23 03:31:12 +0200

    Fixed PUT in CustomerController and CustomerEdit
    Fixed PUT in CustomerController and removed unessessary code from CustomerEdit page.
    Now, all CRUD operations work for Customers
    Note 1: Details for Customers haven't been implemented yet, so we can't see customer's transactions. Also, there are no validations yet in forms, just in the dto's, or error handling for http errors.
    Note 2: Going to proceed with CRUD implementation for the other pages first

--- Session-30/FuelStation/FuelStation.Web.Blazor/Server/Controllers/CustomerController.cs
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Server/Controllers/CustomerController.cs
@@ -43,20 +43,19 @@ namespace FuelStation.Web.Blazor.Server.Controllers {
         }
 
         // POST <CustomerController>
-        [HttpPost]
-       // public async Task Post(CustomerEditDto customer) {            
+        [HttpPost]                 
         public async Task Post(CustomerCreateDto customer) {            
             var newCustomer = new Customer(customer.Name, customer.Surname);           
             _customerRepository.Add(newCustomer);
         }
 
-        // PUT <CustomerController>/450F87A0-9FC8-4C0E-BA65-B45EEFEB9B12
-        [HttpPut("{id}")]
-        public void Put(CustomerEditDto customer) {
+        // PUT <CustomerController>/450F87A0-9FC8-4C0E-BA65-B45EEFEB9B12        
+        [HttpPut]
+        public async Task Put(CustomerEditDto customer) {
             var itemToUpdate = _customerRepository.GetById(customer.Id);
             itemToUpdate.Name = customer.Name;
             itemToUpdate.Surname = customer.Surname;
-            // itemToUpdate.CardNumber = customer.CardNumber; //this is generated, not sure if it is updated by hand
+            itemToUpdate.CardNumber = customer.CardNumber; //this is generated, not sure if it is updated by hand
             _customerRepository.Update(customer.Id, itemToUpdate);
         }
 
