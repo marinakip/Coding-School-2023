85d94239bbf36a3da13fb7e77b0e3abc86d88d00
marinakip <marinakipourou@hotmail.com>
2023-02-24 04:57:59 +0200

    Added annotations to TransactionListDto in Shared

--- Session-30/FuelStation/FuelStation.Web.Blazor/Shared/TransactionDataTranferObjects/TransactionListDto.cs
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Shared/TransactionDataTranferObjects/TransactionListDto.cs
@@ -4,6 +4,7 @@ using FuelStation.Web.Blazor.Shared.EmployeeDataTranferObjects;
 using FuelStation.Web.Blazor.Shared.TransactionLineDataTransferObjects;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,41 @@ using System.Threading.Tasks;
 namespace FuelStation.Web.Blazor.Shared.TransactionDataTranferObjects {
     public class TransactionListDto {
         public Guid Id { get; set; }
-        public DateTime Date { get; set; }       
-        public PaymentMethod PaymentMethod { get; set; }    
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Date is required")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Payment method")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Payment Method is required")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Display(Name = "Total Value")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Total Value is required")]
         public decimal TotalValue { get; set; }
 
+        [Display(Name = "Customer Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Customer Id is required")]
         public int CustomerId { get; set; }
+
+        [Display(Name = "Customer Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Csutomer Name is required")]
         public string CustomerName { get; set; } = null!;
+
+        [Display(Name = "Customer Surname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Customer Surname is required")]
         public string CustomerSurname { get; set; } = null!;       
         public List<CustomerListDto> Customers { get; set; } = new();
 
+        [Display(Name = "Employee Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Employee Id is required")]
         public int EmployeeId { get; set; }
+
+        [Display(Name = "Employee Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Employee Name is required")]
         public string EmployeeName { get; set; } = null!;
+
+        [Display(Name = "Employee Surname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Employee Surname is required")]
         public string EmployeeSurname { get; set; } = null!;        
         public List<EmployeeListDto> Employees { get; set; } = new();
 
