06a382782636eda6783cc50173d5112e95a284d3
marinakip <marinakipourou@hotmail.com>
2023-02-25 09:00:06 +0200

    Added regex validations in Customer DTOs and in Customer forms in pages

--- Session-30/FuelStation/FuelStation.Web.Blazor/Shared/CustomerDataTransferObjects/CustomerCreateDto.cs
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Shared/CustomerDataTransferObjects/CustomerCreateDto.cs
@@ -9,10 +9,12 @@ namespace FuelStation.Web.Blazor.Shared.CustomerDataTransferObjects {
     public class CustomerCreateDto {
         [MaxLength(50, ErrorMessage = "You have exceeded the limit of 50 characters")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        [RegularExpression("^\\p{L}+(?: \\p{L}+)*$", ErrorMessage = "Please give a valid first name")]
         public string Name { get; set; } = null!;
 
         [MaxLength(100, ErrorMessage = "You have exceeded the limit of 100 characters")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Surname is required")]
+        [RegularExpression("^\\p{L}+(?: \\p{L}+)*$", ErrorMessage = "Please give a valid last name")]
         public string Surname { get; set; } = null!;
     }
 }
