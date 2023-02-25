c0514e0b6c6ad13f32c90419f0f988854193d964
marinakip <marinakipourou@hotmail.com>
2023-02-25 23:47:22 +0200

    Added Validations for Item DTOs and pages in Client

--- Session-30/FuelStation/FuelStation.Web.Blazor/Shared/ItemDataTransferObjects/ItemCreateDto.cs
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Shared/ItemDataTransferObjects/ItemCreateDto.cs
@@ -2,29 +2,34 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FuelStation.Web.Blazor.Shared.ItemDataTransferObjects {
     public class ItemCreateDto {
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Description is required")]
-        [RegularExpression("/^[a-zA-Z0-9 ]*$/", ErrorMessage ="Invalid Description. Only letters and numbers are allowed.")]
-        public string Description { get; set; } = null!;
+        [RegularExpression(@"\w.*", ErrorMessage ="Invalid Description.")]
+        public string Description { get; set; } = null!;  //TODO: Needs additional validation to prevent user from sql injections 
 
         [Display(Name = "Item Type")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Item type is required")]
         public ItemType ItemType { get; set; }
 
-        //[RegularExpression(@"^\d{0,3}(\.\d{0,3})?$", ErrorMessage = "Please enter a number for Price with up to 3 decimal places and up to 6 total digits.")]
+        [RegularExpression(@"^\d{0,3}(\,\d{0,3})?$", ErrorMessage = "Enter a valid Price with up to 3 digits integer place and up to 3 decimal place. Use , not . for decimal")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Price is required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0,0.00}")]
         public decimal Price { get; set; }
 
-       // [RegularExpression(@"^\d{0,3}(\.\d{0,3})?$", ErrorMessage = "Please enter a number for Cost with up to 3 decimal places and up to 6 total digits.")]
+        [RegularExpression(@"^\d{0,3}(\,\d{0,3})?$", ErrorMessage = "Enter a valid Cost with up to 3 digits integer place and up to 3 decimal place.Use , not . for decimal")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Cost is required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0,0.00}")]
         public decimal Cost { get; set; }
 
+        //TODO: Custom validator for price when we have type fuel (3 decimals) and other product types (2 decimals)
     }
 }
--- Session-30/FuelStation/FuelStation.Web.Blazor/Shared/ItemDataTransferObjects/ItemEditDto.cs
+++ Session-30/FuelStation/FuelStation.Web.Blazor/Shared/ItemDataTransferObjects/ItemEditDto.cs
@@ -15,20 +15,25 @@ namespace FuelStation.Web.Blazor.Shared.ItemDataTransferObjects {
         public string Code { get; set; } = null!;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Description is required")]
+        [RegularExpression(@"\w.*", ErrorMessage = "Invalid Description.")]   //TODO: Needs additional validation to prevent user from sql injections 
         public string Description { get; set; } = null!;
 
         [Display(Name = "Item Type")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Item type is required")]
         public ItemType ItemType { get; set; }
 
-        //[RegularExpression(@"^\d{0,3}(\.\d{1,3})?$", ErrorMessage = "Please enter a number for Price with up to 3 decimal places and up to 6 total digits.")]
+        [RegularExpression(@"^\d{0,3}(\,\d{0,3})?$", ErrorMessage = "Enter a valid Price with up to 3 digits integer place and up to 3 decimal place. Use , not . for decimal")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Price is required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0,0.00}")]
         public decimal Price { get; set; }
 
-       // [RegularExpression(@"^\d{0,3}(\.\d{1,3})?$", ErrorMessage = "Please enter a number for Cost with up to 3 decimal places and up to 6 total digits.")]
+        [RegularExpression(@"^\d{0,3}(\,\d{0,3})?$", ErrorMessage = "Enter a valid Cost with up to 3 digits integer place and up to 3 decimal place.Use , not . for decimal")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Cost is required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0,0.00}")]
         public decimal Cost { get; set; }
 
-        //TODO: Check again DTOs
+        //TODO: Custom validator for price when we have type fuel (3 decimals) and other product types (2 decimals)
+
     }
 }
+
