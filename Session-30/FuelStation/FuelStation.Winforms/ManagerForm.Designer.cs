1c1ae3fe936729aff5c92ff6b14afa479cb94242
marinakip <marinakipourou@hotmail.com>
2023-02-26 23:22:24 +0200

    Added ManagerForm (not implemented yet)

---
+++ Session-30/FuelStation/FuelStation.Winforms/ManagerForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FuelStation.Winforms {
+    partial class ManagerForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManagerForm));
+            this.btnCashier = new System.Windows.Forms.Button();
+            this.btnStaff = new System.Windows.Forms.Button();
+            this.btnManager = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCashier
+            // 
+            this.btnCashier.BackColor = System.Drawing.Color.Transparent;
+            this.btnCashier.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCashier.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnCashier.FlatAppearance.BorderSize = 2;
+            this.btnCashier.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCashier.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCashier.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCashier.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCashier.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnCashier.Location = new System.Drawing.Point(325, 281);
+            this.btnCashier.Name = "btnCashier";
+            this.btnCashier.Size = new System.Drawing.Size(107, 39);
+            this.btnCashier.TabIndex = 7;
+            this.btnCashier.Text = "Transactions";
+            this.btnCashier.UseVisualStyleBackColor = false;
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.BackColor = System.Drawing.Color.Transparent;
+            this.btnStaff.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnStaff.FlatAppearance.BorderColor = System.Drawing.Color.PeachPuff;
+            this.btnStaff.FlatAppearance.BorderSize = 2;
+            this.btnStaff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStaff.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStaff.ForeColor = System.Drawing.Color.PeachPuff;
+            this.btnStaff.Location = new System.Drawing.Point(177, 281);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(107, 39);
+            this.btnStaff.TabIndex = 6;
+            this.btnStaff.Text = "Items";
+            this.btnStaff.UseVisualStyleBackColor = false;
+            // 
+            // btnManager
+            // 
+            this.btnManager.BackColor = System.Drawing.Color.Transparent;
+            this.btnManager.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnManager.FlatAppearance.BorderColor = System.Drawing.Color.GhostWhite;
+            this.btnManager.FlatAppearance.BorderSize = 2;
+            this.btnManager.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManager.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnManager.ForeColor = System.Drawing.Color.Snow;
+            this.btnManager.Location = new System.Drawing.Point(33, 281);
+            this.btnManager.Name = "btnManager";
+            this.btnManager.Size = new System.Drawing.Size(107, 39);
+            this.btnManager.TabIndex = 5;
+            this.btnManager.Text = "Customers";
+            this.btnManager.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(469, 351);
+            this.Controls.Add(this.btnCashier);
+            this.Controls.Add(this.btnStaff);
+            this.Controls.Add(this.btnManager);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ManagerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnCashier;
+        private Button btnStaff;
+        private Button btnManager;
+        private PictureBox pictureBox1;
+    }
+}
