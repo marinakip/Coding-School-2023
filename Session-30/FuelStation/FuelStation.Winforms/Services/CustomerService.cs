29a63af7595555e5e71f3b071bb9c684bb8d8906
marinakip <marinakipourou@hotmail.com>
2023-02-27 04:44:45 +0200

    Added CustomerService that handles HttpClient requests
    CustomerService has methods GetCustomers, GetCustomerById, AddCustomer, UpdateCustomer and DeleteCustomer that do httpclient requests to the Blazor Server
    NOTE: Haven't been tried yet, they might need modifications

---
+++ Session-30/FuelStation/FuelStation.Winforms/Services/CustomerService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using FuelStation.Model.Entities;
+using FuelStation.Web.Blazor.Shared.CustomerDataTransferObjects;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using Microsoft.JSInterop;
+
+namespace FuelStation.Winforms.Services {
+    public class CustomerService {
+        private readonly HttpClient _httpClient;
+        public CustomerService() {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7292/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<IEnumerable<CustomerListDto>> GetCustomers() {
+
+            try {
+                List<CustomerListDto> customerList = await _httpClient.GetFromJsonAsync<List<CustomerListDto>>("customer");
+                return customerList;
+            } catch (Exception ex) {
+                //TODO: Go to error page
+                //TODO: Implement Error Page
+                MessageBox.Show($"Error in GetCustomers: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<CustomerEditDto> GetCustomerById(Guid Id) {
+            try {
+               CustomerEditDto customer = await _httpClient.GetFromJsonAsync<CustomerEditDto>($"customer/{Id}");
+                return customer;
+            } catch (Exception ex) {
+                MessageBox.Show($"Error in GetCustomerById: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task AddCustomer(CustomerCreateDto customer) {
+            HttpResponseMessage? response = null;
+            response = await _httpClient.PostAsJsonAsync("customer", customer);
+            try {
+                response.EnsureSuccessStatusCode();                
+            } catch (Exception ex) {
+                MessageBox.Show($"Error in AddCustomer: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task UpdateCustomer(CustomerEditDto customer) {
+            HttpResponseMessage? response = null;
+            response = await _httpClient.PutAsJsonAsync("customer", customer);
+            try {
+                response.EnsureSuccessStatusCode();
+            } catch (Exception ex) {
+                MessageBox.Show($"Error in AddCustomer: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private async Task DeleteCustomer(CustomerListDto customer) {
+            DialogResult result = MessageBox.Show($"Delete customer {customer.Name} {customer.Surname}. Are you sure?", "Confirmation", MessageBoxButtons.OKCancel);
+           
+            if (result == DialogResult.OK) {                               
+                try {
+                    var response = await _httpClient.DeleteAsync($"customer/{customer.Id}");
+                    response.EnsureSuccessStatusCode();
+                } catch (Exception ex) {
+                    MessageBox.Show($"Error in DeleteCustomer: {ex.Message}");
+                    throw new Exception(ex.Message);
+                }
+            } else {
+                MessageBox.Show($"Delete Customer was Canceled");
+            }
+        }
+
+    }
+}
