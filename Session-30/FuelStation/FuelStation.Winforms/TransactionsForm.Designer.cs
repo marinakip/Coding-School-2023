de5c5aedbc8e0cbf408e1eb153fd6fea7be3dd20
marinakip <marinakipourou@hotmail.com>
2023-03-02 05:54:54 +0200

    Tried TransactionsForm - still needs work

--- Session-30/FuelStation/FuelStation.Winforms/TransactionsForm.Designer.cs
+++ Session-30/FuelStation/FuelStation.Winforms/TransactionsForm.Designer.cs
@@ -23,9 +23,10 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransactionsForm));
             this.lbTransactions = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grvTransactions = new System.Windows.Forms.DataGridView();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnOK = new System.Windows.Forms.Button();
@@ -34,9 +35,12 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnSearchCustomer = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnRefreshList = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grvTransactions)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbTransactions
@@ -50,14 +54,15 @@
             this.lbTransactions.TabIndex = 16;
             this.lbTransactions.Text = "Transactions";
             // 
-            // dataGridView1
+            // grvTransactions
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(27, 111);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(739, 220);
-            this.dataGridView1.TabIndex = 15;
+            this.grvTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvTransactions.Location = new System.Drawing.Point(27, 111);
+            this.grvTransactions.Name = "grvTransactions";
+            this.grvTransactions.RowTemplate.Height = 25;
+            this.grvTransactions.Size = new System.Drawing.Size(739, 220);
+            this.grvTransactions.TabIndex = 15;
+            this.grvTransactions.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // pictureBox2
             // 
@@ -95,6 +100,7 @@
             this.btnOK.TabIndex = 23;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // btnCancel
             // 
@@ -109,6 +115,7 @@
             this.btnCancel.TabIndex = 22;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnAdd
             // 
@@ -123,6 +130,7 @@
             this.btnAdd.TabIndex = 21;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
@@ -137,6 +145,7 @@
             this.btnDelete.TabIndex = 20;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -151,6 +160,7 @@
             this.btnUpdate.TabIndex = 19;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnSearchCustomer
             // 
@@ -165,6 +175,22 @@
             this.btnSearchCustomer.TabIndex = 24;
             this.btnSearchCustomer.Text = "Search Customer";
             this.btnSearchCustomer.UseVisualStyleBackColor = false;
+            this.btnSearchCustomer.Click += new System.EventHandler(this.btnSearchCustomer_Click);
+            // 
+            // btnRefreshList
+            // 
+            this.btnRefreshList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnRefreshList.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRefreshList.FlatAppearance.BorderSize = 2;
+            this.btnRefreshList.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRefreshList.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRefreshList.Location = new System.Drawing.Point(647, 80);
+            this.btnRefreshList.Name = "btnRefreshList";
+            this.btnRefreshList.Size = new System.Drawing.Size(119, 29);
+            this.btnRefreshList.TabIndex = 25;
+            this.btnRefreshList.Text = "Refresh List";
+            this.btnRefreshList.UseVisualStyleBackColor = false;
+            this.btnRefreshList.Click += new System.EventHandler(this.btnRefreshList_Click);
             // 
             // TransactionsForm
             // 
@@ -172,6 +198,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefreshList);
             this.Controls.Add(this.btnSearchCustomer);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.btnCancel);
@@ -179,14 +206,16 @@
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.lbTransactions);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grvTransactions);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "TransactionsForm";
             this.Text = "Fuel Pulse Station - Transactions";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.TransactionsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grvTransactions)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,7 +224,7 @@
         #endregion
 
         private Label lbTransactions;
-        private DataGridView dataGridView1;
+        private DataGridView grvTransactions;
         private PictureBox pictureBox2;
         private PictureBox pictureBox1;
         private Button btnOK;
@@ -204,5 +233,7 @@
         private Button btnDelete;
         private Button btnUpdate;
         private Button btnSearchCustomer;
+        private Button btnRefreshList;
+        private BindingSource bindingSource1;
     }
 }
--- Session-30/FuelStation/FuelStation.Winforms/TransactionsForm.cs
+++ Session-30/FuelStation/FuelStation.Winforms/TransactionsForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FuelStation.Web.Blazor.Shared.TransactionDataTranferObjects;
+using FuelStation.Winforms.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,8 +12,78 @@ using System.Windows.Forms;
 
 namespace FuelStation.Winforms {
     public partial class TransactionsForm : Form {
+        private TransactionService _transactionService = new();
         public TransactionsForm() {
             InitializeComponent();
         }
+
+
+        private async void TransactionsForm_Load(object sender, EventArgs e) {
+            await LoadTransactionsAsync();
+        }
+
+        private async Task LoadTransactionsAsync() {
+            try {
+                IEnumerable<TransactionListDto> transactions = await _transactionService.GetTransactions();
+                grvTransactions.DataSource = transactions.ToList();
+            } catch (Exception ex) {
+                MessageBox.Show($"Error loading transactions: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void btnSearchCustomer_Click(object sender, EventArgs e) {
+            //TODO SEARCH CUSTOMER
+        }
+
+        private void btnRefreshList_Click(object sender, EventArgs e) {
+            RefreshTransactionList();
+        }
+
+        private async void RefreshTransactionList() {
+            IEnumerable<TransactionListDto> transactions = await _transactionService.GetTransactions();
+            grvTransactions.DataSource = transactions.ToList();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e) {
+            TransactionListDto selectedTransaction = GetSelectedTransaction();
+            UpdateTransactionForm updateTransactionForm = new UpdateTransactionForm(selectedTransaction);
+            updateTransactionForm.ShowDialog();
+        }
+
+        private TransactionListDto GetSelectedTransaction() {
+            try {
+                DataGridViewRow selectedRow = grvTransactions.SelectedRows[0];
+                TransactionListDto selectedTransaction = (TransactionListDto)selectedRow.DataBoundItem;
+                return selectedTransaction;
+            } catch (Exception ex) {
+                MessageBox.Show("You should select a row first");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e) {
+            TransactionListDto selectedTransaction = GetSelectedTransaction();
+            await _transactionService.DeleteTransaction(selectedTransaction);
+            RefreshTransactionList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            CreateTransactionForm createTransactionForm = new CreateTransactionForm();
+            createTransactionForm.ShowDialog();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
     }
 }
