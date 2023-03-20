4b7e71122e00aa95b8c5bae0c164184b981de984
marinakip <marinakipourou@hotmail.com>
2023-03-20 03:25:01 +0200

    Add UpdateTransactionLineForm (design only, not yet implemented)

---
+++ Session-30/FuelStation/FuelStation.Winforms/UpdateTransactionLineForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace FuelStation.Winforms {
+    partial class UpdateTransactionLineForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateTransactionLineForm));
+            this.textBoxTotalValue = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxDiscountValue = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxDiscountPercent = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxItemPrice = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxItemCode = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxQuantity = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxItemDescription = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTotalValue
+            // 
+            this.textBoxTotalValue.Location = new System.Drawing.Point(199, 242);
+            this.textBoxTotalValue.Name = "textBoxTotalValue";
+            this.textBoxTotalValue.Size = new System.Drawing.Size(134, 23);
+            this.textBoxTotalValue.TabIndex = 90;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label10.Location = new System.Drawing.Point(41, 242);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(88, 19);
+            this.label10.TabIndex = 89;
+            this.label10.Text = "Total Value:";
+            // 
+            // textBoxDiscountValue
+            // 
+            this.textBoxDiscountValue.Location = new System.Drawing.Point(613, 198);
+            this.textBoxDiscountValue.Name = "textBoxDiscountValue";
+            this.textBoxDiscountValue.Size = new System.Drawing.Size(91, 23);
+            this.textBoxDiscountValue.TabIndex = 88;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label9.Location = new System.Drawing.Point(481, 202);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(114, 19);
+            this.label9.TabIndex = 87;
+            this.label9.Text = "Discount Value:";
+            // 
+            // textBoxDiscountPercent
+            // 
+            this.textBoxDiscountPercent.Location = new System.Drawing.Point(199, 198);
+            this.textBoxDiscountPercent.Name = "textBoxDiscountPercent";
+            this.textBoxDiscountPercent.Size = new System.Drawing.Size(134, 23);
+            this.textBoxDiscountPercent.TabIndex = 86;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(41, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 19);
+            this.label4.TabIndex = 85;
+            this.label4.Text = "Discount Percent:";
+            // 
+            // textBoxItemPrice
+            // 
+            this.textBoxItemPrice.Location = new System.Drawing.Point(613, 156);
+            this.textBoxItemPrice.Name = "textBoxItemPrice";
+            this.textBoxItemPrice.Size = new System.Drawing.Size(91, 23);
+            this.textBoxItemPrice.TabIndex = 84;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label7.Location = new System.Drawing.Point(513, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 19);
+            this.label7.TabIndex = 83;
+            this.label7.Text = "Item Price:";
+            // 
+            // textBoxItemCode
+            // 
+            this.textBoxItemCode.Location = new System.Drawing.Point(613, 116);
+            this.textBoxItemCode.Name = "textBoxItemCode";
+            this.textBoxItemCode.Size = new System.Drawing.Size(176, 23);
+            this.textBoxItemCode.TabIndex = 82;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label6.Location = new System.Drawing.Point(513, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 19);
+            this.label6.TabIndex = 81;
+            this.label6.Text = "Item Code:";
+            // 
+            // comboBoxQuantity
+            // 
+            this.comboBoxQuantity.FormattingEnabled = true;
+            this.comboBoxQuantity.Location = new System.Drawing.Point(199, 156);
+            this.comboBoxQuantity.Name = "comboBoxQuantity";
+            this.comboBoxQuantity.Size = new System.Drawing.Size(134, 23);
+            this.comboBoxQuantity.TabIndex = 80;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(41, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 79;
+            this.label2.Text = "Quantity:";
+            // 
+            // comboBoxItemDescription
+            // 
+            this.comboBoxItemDescription.FormattingEnabled = true;
+            this.comboBoxItemDescription.Location = new System.Drawing.Point(199, 116);
+            this.comboBoxItemDescription.Name = "comboBoxItemDescription";
+            this.comboBoxItemDescription.Size = new System.Drawing.Size(245, 23);
+            this.comboBoxItemDescription.TabIndex = 78;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(41, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 19);
+            this.label3.TabIndex = 77;
+            this.label3.Text = "Item Description:";
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnOK.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnOK.FlatAppearance.BorderSize = 2;
+            this.btnOK.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnOK.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnOK.Location = new System.Drawing.Point(653, 285);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(91, 29);
+            this.btnOK.TabIndex = 76;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCancel.FlatAppearance.BorderSize = 2;
+            this.btnCancel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCancel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCancel.Location = new System.Drawing.Point(750, 285);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(91, 29);
+            this.btnCancel.TabIndex = 75;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(327, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 26);
+            this.label1.TabIndex = 74;
+            this.label1.Text = "Update Transaction Line";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(752, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(105, 48);
+            this.pictureBox2.TabIndex = 73;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 72;
+            this.pictureBox1.TabStop = false;
+            // 
+            // UpdateTransactionLineForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(856, 321);
+            this.Controls.Add(this.textBoxTotalValue);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxDiscountValue);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxDiscountPercent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxItemPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxItemCode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBoxQuantity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxItemDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "UpdateTransactionLineForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fuel Pulse Station - Update Transaction Line";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxTotalValue;
+        private Label label10;
+        private TextBox textBoxDiscountValue;
+        private Label label9;
+        private TextBox textBoxDiscountPercent;
+        private Label label4;
+        private TextBox textBoxItemPrice;
+        private Label label7;
+        private TextBox textBoxItemCode;
+        private Label label6;
+        private ComboBox comboBoxQuantity;
+        private Label label2;
+        private ComboBox comboBoxItemDescription;
+        private Label label3;
+        private Button btnOK;
+        private Button btnCancel;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+    }
+}
